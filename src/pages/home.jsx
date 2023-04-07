e3f8361b2fdcdff194275f665527492385cd5b39
ishak-dev <ishak.kazic@gmail.com>
2023-04-07 12:30:35 +0200

    logout btn

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <button
+        onClick={() => {
+          localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Home;
